060869ee86c34f714cd0eb1f5480d86ec5cdd576
Princess Alfiona Dwiratysa <fionadwira@gmail.com>
2026-08-04 09:37:47 +0700

    Membuat halaman home

---
+++ home/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+
+    <h1>Selamat Datang di Website Kami</h1>
+    <p>Ini adalah halaman home.</p>
+
+    <h3>Kelompok</h3>
+    <p>Princess Alfiona & Maylian</p>
+
+</body>
+</html>
